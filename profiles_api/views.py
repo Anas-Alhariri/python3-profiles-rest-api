0506ac46cb1171aa1f0375804efce041f98391c3
Anas Alhariri (Max) <eng.anas.alhariri@gmail.com>
2022-06-08 20:40:46 +0000

    Added Profile View Set with protection from users changing otherther users profiles.

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,12 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
-
 from profiles_api import models
+from profiles_api import permissions
 
 # Create your views here.
 class HelloApiView(APIView):
@@ -101,7 +101,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
-
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
     # def get_permissions(self):
     #     """Handle permissions"""
     #     if self.request.method in permissions.SAFE_METHODS:
